9a4e8d977ba0d620f9518c973044e1d356cd06cf
EkaterinaBudilova <ekaterinazh1@mail.ru>
2018-02-27 09:26:27 +1200

    исправлены ошибки в 1 задаче

--- slider/js/slider.js
+++ slider/js/slider.js
@@ -1,27 +1,53 @@
 const slides = document.getElementsByClassName('slide');
 let currSlide = slides[0];
-currSlide.classList.add('slide-current');
 const sliderNav = document.getElementsByClassName('slider-nav');
-
 const controls = sliderNav[0].children;
+
+currSlide.classList.add('slide-current');
 
 function isFirst(slide) {
   if (slide === slides[0]) {
     controls[0].classList.add('disabled');
     controls[2].classList.add('disabled');
   }
+  else {
+    controls[0].classList.remove('disabled');
+    controls[2].classList.remove('disabled');
+  }
 }
+
+function isLast(slide) {
+  if (slide === slides[slides.length-1]) {
+    controls[1].classList.add('disabled');
+    controls[3].classList.add('disabled');
+  }
+  else {
+    controls[1].classList.remove('disabled');
+    controls[3].classList.remove('disabled');
+  }
+}
+
 isFirst(currSlide);
+isLast(currSlide);
+
 function act(event) {
   currSlide.classList.remove('slide-current');
   switch (event.target.dataset.action) {
-    case 'prev': curr_slide = curr_slide.previousElementSibling; 
-    case 'next': curr_slide = curr_slide.nextElementSibling;
-    case 'first': curr_slide = slides[0];
-    case 'last': curr_slide = slides[slides.length-1];   
+    case 'prev': 
+      console.log('prev');
+      currSlide = currSlide.previousElementSibling; 
+      break;
+    
+    case 'next': currSlide = currSlide.nextElementSibling;
+      break;
+    case 'first': currSlide = slides[0];
+      break;
+    case 'last': currSlide = slides[slides.length-1];   
+      break;
   }
-  curr_slide.classList.add('slide-current');
+  currSlide.classList.add('slide-current');
   isFirst(currSlide);
+  isLast(currSlide);
 }
 
 Array.from(controls).forEach(item => item.addEventListener('click', act));
